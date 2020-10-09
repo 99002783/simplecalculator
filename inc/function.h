d873ee2ba5b26a0c5a94e9e69e37a3bb9675c80e
99002783 <72496295+99002783@users.noreply.github.com>
2020-10-09 15:47:07 +0530

    Update function.h

--- inc/function.h
+++ inc/function.h
@@ -1,1 +1,13 @@
 
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#include <stdio.h>
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+#endif
